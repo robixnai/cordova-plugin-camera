5bfcaef8b03d69008e9869e1496943b336ecc174
robixnai <moreira_rob@hotmail.com>
2017-05-12 16:07:22 -0300

    Update FileHelper.java

--- src/android/FileHelper.java
+++ src/android/FileHelper.java
@@ -256,20 +256,13 @@ public class FileHelper {
      * @return The value of the _data column, which is typically a file path.
      * @author paulburke
      */
-    public static String getDataColumn(Context context, Uri uri, String selection,
-                                       String[] selectionArgs) {
-
+    public static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
         Cursor cursor = null;
-        final String column = "_data";
-        final String[] projection = {
-                column
-        };
-
+        final String column = MediaStore.Images.Media.DATA;
+        final String[] projection = { column };
         try {
-            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
-                    null);
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
             if (cursor != null && cursor.moveToFirst()) {
-
                 final int column_index = cursor.getColumnIndexOrThrow(column);
                 return cursor.getString(column_index);
             }
